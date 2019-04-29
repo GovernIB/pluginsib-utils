eb4c022a00bf21fef04c8afc30194fe84610eea5
anadal-fundaciobit <anadal@fundaciobit.org>
2019-04-29 11:51:13 +0200

    JBOSS 7: fer un nou plugin de validacio de firmes via @firmaCXF per JBOSS 7 (https://github.com/GovernIB/portafib/issues/256) (https://github.com/GovernIB/pluginsib-validatesignature/issues/13)

---
+++ cxf-jboss7/src/test/java/org/fundaciobit/pluginsib/utils/cxf/test/CXFUtilsTester.java
@@ -0,0 +1,47 @@
+package org.fundaciobit.pluginsib.utils.cxf.test;
+
+import java.io.InputStream;
+
+import org.fundaciobit.pluginsib.core.utils.FileUtils;
+import org.fundaciobit.pluginsib.utils.cxf.CXFUtils;
+
+/**
+ * 
+ * @author anadal
+ *
+ */
+public class CXFUtilsTester {
+
+  public static void main(String[] args) {
+
+    try {
+
+      InputStream is = FileUtils.readResource(CXFUtilsTester.class, "ORVE_firma0.xsig");
+
+      byte[] data = FileUtils.toByteArray(is);
+
+      System.out.println(CXFUtils.isXMLFormat(data));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    
+    try {
+
+      InputStream is = FileUtils.readResource(CXFUtilsTester.class, "firma0.xsig");
+
+      byte[] data = FileUtils.toByteArray(is);
+
+      System.out.println(CXFUtils.isXMLFormat(data));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    
+    
+
+  }
+
+}
