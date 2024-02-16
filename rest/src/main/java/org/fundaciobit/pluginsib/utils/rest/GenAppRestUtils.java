3cb6c2040adca479012b786ba6775e1b827b797d
Antoni Nadal <anadal@fundaciobit.org>
2024-02-16 07:44:47 +0100

    Canvis i Afegits en Estructures de DadesObertes https://github.com/GovernIB/pluginsib-utils/issues/8

--- rest/src/main/java/org/fundaciobit/pluginsib/utils/rest/GenAppRestUtils.java
+++ rest/src/main/java/org/fundaciobit/pluginsib/utils/rest/GenAppRestUtils.java
@@ -1,7 +1,6 @@
 package org.fundaciobit.pluginsib.utils.rest;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,8 +13,8 @@ import org.fundaciobit.genapp.common.i18n.I18NException;
 import org.fundaciobit.genapp.common.query.Field;
 import org.fundaciobit.genapp.common.query.ITableManager;
 import org.fundaciobit.genapp.common.query.OrderBy;
+import org.fundaciobit.genapp.common.query.Select;
 import org.fundaciobit.genapp.common.query.Where;
-
 
 /**
  * 
@@ -25,14 +24,10 @@ import org.fundaciobit.genapp.common.query.Where;
 public class GenAppRestUtils {
 
     private static Logger log = Logger.getLogger(GenAppRestUtils.class);
-    
-    
 
-    
     public static <D extends IGenAppEntity, P extends AbstractPagination<D>> P createRestPagination(Class<P> classe,
             ITableManager<D, Long> ejb, int page, int pagesize, Where w, OrderBy orderBy) throws I18NException {
-        
-        
+
         GenAppEntityConverter<D, D> converter = new GenAppEntityConverter<D, D>() {
 
             @Override
@@ -40,13 +35,11 @@ public class GenAppRestUtils {
                 return genAppEntity;
             }
         };
-        
-        
+
         return createRestPagination(classe, ejb, page, pagesize, w, orderBy, converter);
-        
+
     }
-    
-    
+
     /**
      * 
      * @param <D>  Entitat del Model GenApp
@@ -62,24 +55,22 @@ public class GenAppRestUtils {
      * @throws I18NException 
      */
     public static <R, D extends IGenAppEntity, P extends AbstractPagination<R>> P createRestPagination(Class<P> classe,
-            ITableManager<D, Long> ejb, int page, int pagesize, Where w, OrderBy orderBy, GenAppEntityConverter<D, R> converter, String name, CharSequence nextQuery) throws I18NException {
+            ITableManager<D, Long> ejb, int page, int pagesize, Where w, OrderBy orderBy,
+            GenAppEntityConverter<D, R> converter, String name, CharSequence nextQuery) throws I18NException {
 
-        
-        P pag = createRestPagination(classe,
-                ejb, page, pagesize, w, orderBy, converter);
-        
+        P pag = createRestPagination(classe, ejb, page, pagesize, w, orderBy, converter);
+
         if (pag instanceof ReuseDataPagination) {
-            ReuseDataPagination<R> paginat = (ReuseDataPagination<R>)pag;
+            ReuseDataPagination<R> paginat = (ReuseDataPagination<R>) pag;
             paginat.setName(name);
             paginat.setDateDownload(RestUtils.convertDateToDateTimeISO8601(new Date()));
-            paginat.setNextUrl((pag.getPage() >= pag.getTotalpages())?null:nextQuery.toString());
+            paginat.setNextUrl((pag.getPage() >= pag.getTotalpages()) ? null : nextQuery.toString());
+            paginat.setItemsReturned(paginat.getData().size());
         }
-    
+
         return pag;
     }
-    
-    
-    
+
     /**
      * 
      * @param <D>  Entitat del Model GenApp
@@ -95,14 +86,15 @@ public class GenAppRestUtils {
      * @throws I18NException 
      */
     public static <R, D extends IGenAppEntity, P extends AbstractPagination<R>> P createRestPagination(Class<P> classe,
-            ITableManager<D, Long> ejb, int page, int pagesize, Where w, OrderBy orderBy, GenAppEntityConverter<D, R> converter) throws I18NException {
+            ITableManager<D, Long> ejb, int page, int pagesize, Where w, OrderBy orderBy,
+            GenAppEntityConverter<D, R> converter) throws I18NException {
 
         final int firstResult = (page - 1) * pagesize;
         final int maxResults = pagesize;
         final List<D> llistatBBDD = ejb.select(w, null, firstResult, maxResults, orderBy);
 
         final List<R> llistat = new ArrayList<R>();
-        
+
         for (D d : llistatBBDD) {
             llistat.add(converter.convert(d));
         }
@@ -130,8 +122,6 @@ public class GenAppRestUtils {
         return paginacio;
     }
 
-
-    
     /**
      *  
       * 
@@ -145,15 +135,13 @@ public class GenAppRestUtils {
       * @return Array de dates on [0] és datestart i [1] es dateend
      * @throws RestException
      */
-     public static <T extends Date> GenAppRangeOfDates checkRangeOfOnlyDates(final String dataIniciRequest,
-             final String dataIniciRequestLabel, final String dataFiRequest, final String dataFiRequestLabel,
-             final Field<T> dataField, String language) throws RestException {
-         /*return checkRangeOfOnlyDates(dataIniciRequest,
-                 dataIniciRequestLabel,  dataFiRequest, dataFiRequestLabel,
-                 dataField, null , language); */
-         return new CheckRangeOfOnlyDates<T>().checkRangeOfOnlyDates(dataIniciRequest, dataIniciRequestLabel,
-                 dataFiRequest, dataFiRequestLabel, dataField, null, language);
-     }
+    public static <T extends Date> GenAppRangeOfDates checkRangeOfOnlyDates(final String dataIniciRequest,
+            final String dataIniciRequestLabel, final String dataFiRequest, final String dataFiRequestLabel,
+            final Field<T> dataField, String language) throws RestException {
+
+        return checkRangeOfOnlyDates(dataIniciRequest, dataIniciRequestLabel, dataFiRequest, dataFiRequestLabel,
+                dataField, null, language);
+    }
 
     /**
     *  
@@ -171,74 +159,58 @@ public class GenAppRestUtils {
     public static <T extends Date> GenAppRangeOfDates checkRangeOfOnlyDates(final String dataIniciRequest,
             final String dataIniciRequestLabel, final String dataFiRequest, final String dataFiRequestLabel,
             final Field<T> dataField, StringBuilder nextQuery, String language) throws RestException {
-        return new CheckRangeOfOnlyDates<T>().checkRangeOfOnlyDates(dataIniciRequest, dataIniciRequestLabel,
-                dataFiRequest, dataFiRequestLabel, dataField, nextQuery, language);
+
+        final Date[] dates = RestUtils.checkRangeOfOnlyDates(dataIniciRequest, dataIniciRequestLabel, dataFiRequest,
+                dataFiRequestLabel, language);
+
+        Date startDate;
+        Date endDate;
+
+        startDate = dates[0];
+        endDate = dates[1];
+
+        Where w = null;
+        if (startDate != null) {
+            if (dataField != null) {
+                w = dataField.greaterThanOrEqual(createFromDate(dataField, startDate));
+            }
+            if (nextQuery != null) {
+                nextQuery.append("&" + dataIniciRequestLabel + "=" + dataIniciRequest);
+            }
+        }
+
+        if (endDate != null) {
+            if (dataField != null) {
+                Where ed = dataField.lessThanOrEqual(createFromDate(dataField, endDate));
+                w = (w == null) ? ed : Where.AND(w, ed);
+            }
+            if (nextQuery != null) {
+                nextQuery.append("&" + dataFiRequestLabel + "=" + dataFiRequest);
+            }
+        }
+
+        return new GenAppRangeOfDates(startDate, endDate, w);
+
     }
 
-    /**
-     * 
-     * @author anadal
-     *
-     * @param <T>
-     */
-    protected static class CheckRangeOfOnlyDates<T extends Date> {
+    private static <T> T createFromDate(Field<T> field, Date d) throws RestException {
 
-        //@SuppressWarnings("unchecked")
-        public Class<T> getTypeParameterClass() {
-            Type type = getClass().getGenericSuperclass();
-            ParameterizedType paramType = (ParameterizedType) type;
-            return (Class<T>) paramType.getActualTypeArguments()[0];
+        try {
+
+            Select<T> select = field.select;
+
+            Class<?> enclosingClass = select.getClass();
+
+            Method method = enclosingClass.getDeclaredMethod("getFromObject", Object.class);
+
+            Class<T> clazz = (Class<T>) method.getReturnType();
+
+            T t = clazz.getConstructor(Long.TYPE).newInstance(d.getTime());
+            return t;
+        } catch (Throwable e) {
+            final String msg = "Error instanciant class " + field + ": " + e.getMessage();
+            throw new RestException(msg, Status.BAD_REQUEST);
         }
-
-        public GenAppRangeOfDates checkRangeOfOnlyDates(final String dataIniciRequest,
-                final String dataIniciRequestLabel, final String dataFiRequest, final String dataFiRequestLabel,
-                final Field<T> dataField, StringBuilder nextQuery, String language) throws RestException {
-
-            final Date[] dates = RestUtils.checkRangeOfOnlyDates(dataIniciRequest, dataIniciRequestLabel, dataFiRequest,
-                    dataFiRequestLabel, language);
-
-            Date startDate;
-            Date endDate;
-
-            startDate = dates[0];
-            endDate = dates[1];
-
-            Where w = null;
-            if (startDate != null) {
-                if (dataField != null) {
-                  w = dataField.greaterThanOrEqual(createFromDate(startDate));
-                }
-                if (nextQuery != null) {
-                  nextQuery.append("&" + dataIniciRequestLabel + "=" + dataIniciRequest);
-                }
-            }
-
-            if (endDate != null) {
-                if (dataField != null) {
-                  Where ed = dataField.lessThanOrEqual(createFromDate(endDate));
-                  w = (w == null) ? ed : Where.AND(w, ed);
-                }
-                if (nextQuery != null) {
-                  nextQuery.append("&" + dataFiRequestLabel + "=" + dataFiRequest);
-                }
-            }
-
-            return new GenAppRangeOfDates(startDate, endDate, w);
-
-        }
-
-        private T createFromDate(Date d) {
-            Class<T> clazz = getTypeParameterClass();
-
-            try {
-                T t = clazz.getDeclaredConstructor().newInstance();
-                t.setTime(d.getTime());
-                return t;
-            } catch (Throwable e) {
-                final String msg = "Error instanciant class " + clazz + ": " + e.getMessage();
-                throw new RestException(msg, Status.BAD_REQUEST);
-            }
-        }
-
     }
+
 }
